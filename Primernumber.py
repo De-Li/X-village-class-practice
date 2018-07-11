a0bebe4b2ecb59cb863ec805bc5493e6de041b6b
jeff <jeff>
2018-07-11 11:50:33 +0800

    for use list

--- Primernumber.py
+++ Primernumber.py
@@ -3,9 +3,8 @@ prime = [2,3,5,7]
 x=int(input("enter a range: "))
 while i<x:
     temp=0
-    j=0
-    for j in range(len(prime)):
-     if i%prime[j]==0:
+    for j in prime:
+     if i%j==0:
          temp+=1
     if temp<=1:
         print("prime number = ",i)
